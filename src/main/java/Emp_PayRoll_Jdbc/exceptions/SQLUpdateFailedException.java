a0f6da76e489a4478bf922069d1cd506acd88ce9
anikeshmahajan <36470775+anikeshmahajan@users.noreply.github.com>
2021-05-20 13:02:54 +0530

    Delete src directory

--- src/main/java/Emp_PayRoll_Jdbc/exceptions/SQLUpdateFailedException.java
+++
@@ -1,7 +0,0 @@
-package Emp_PayRoll_Jdbc.exceptions;
-
-public class SQLUpdateFailedException extends Throwable {
-    public SQLUpdateFailedException(String message) {
-        super(message);
-    }
-}
